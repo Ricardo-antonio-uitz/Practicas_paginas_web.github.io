a44d3c1251187c348c64b6c7eaeff7384d19058c
Ricardo Antonio Uitz Koyoc <l21530379@cancun.tecnm.mx>
2024-11-06 20:43:56 -0500

    Add files via upload

---
+++ Practicas/practica1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Practica1 de php</title>
+<body>
+<?php
+echo "hola este es mi primeer script en PHP<BR>";
+//este es una linea de comentario 
+# esto Tambien es una linea de comentario
+/* esto es un bloque de mas de 
+una linea de comentario */
+
+$vartext="hola! ahora estoy usando variables para guardar texto<BR>";
+echo $vartext;
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "el resultado de la suma de $varnum1 y $varnum2 es = $res"
+?>
+</body>
+</html>
