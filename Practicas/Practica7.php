6f60928b7a3a5ebc305bbd00af6ecdc1725a6f84
Ricardo Antonio Uitz Koyoc <l21530379@cancun.tecnm.mx>
2024-11-16 09:39:07 -0500

    Add files via upload

---
+++ Practicas/Practica7.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 7</title>
+</head>
+<body>
+    <h1>Practica 7</h1>
+    
+    <img src="Practica7.png" alt="Practica7:Creacion de Base de datos con PHP" width="500">
+</body>
+</html>
